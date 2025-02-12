eb90098fc49e1abea6643ee6f16a46875f670367
crisjimen <182010261+crisjimen@users.noreply.github.com>
2025-02-12 20:53:31 +0100

    Implementación de todas la secciones del register, aún pendientes de otras funciones en JavaScript y del diseño responsive

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ window.onload = () => {
     }
 };
 
+//EL cursor lleva un pequeño círculo que lo sigue
 document.addEventListener("mousemove", function(e) {
     const cursor = document.querySelector('.cursor');
 
@@ -23,7 +24,8 @@ document.addEventListener("mousemove", function(e) {
     });
 });
 
-document.querySelectorAll("input, textarea").forEach((element) => {
+//Quitar el cursor cuando entra a un input o a un button
+document.querySelectorAll("input,button").forEach((element) => {
     element.addEventListener("mouseenter", () => {
         document.querySelector(".cursor").style.opacity = "0";
     });
@@ -31,3 +33,40 @@ document.querySelectorAll("input, textarea").forEach((element) => {
         document.querySelector(".cursor").style.opacity = "1";
     });
 });
+
+//Mostrar diferentes partes del documento según la sección en la que se clica
+const navLinks = document.querySelectorAll('.form__sidebar h2');
+
+const forms = document.querySelectorAll('.form__main')
+
+navLinks.forEach(navLink => {
+
+    navLink.addEventListener('click', (e) => {
+    //Se despliegan diferentes tarjetas dependiendo del link que se clique
+    e.preventDefault();
+    navLinks.forEach(link => link.classList.remove('selected'));
+    e.target.classList.add('selected');
+
+    if(e.target.textContent.includes('Basic \nInformation')){
+        forms[0].style.display='flex';
+        forms[1].style.display='none';
+        forms[2].style.display='none';
+    }
+
+    if(e.target.textContent.includes('Personal \nIdentification')){
+        forms[0].style.display='none';
+        forms[1].style.display='flex';
+        forms[2].style.display='none';
+    }
+
+    if(e.target.textContent.includes('Security \n& Payment')){
+        forms[0].style.display='none';
+        forms[1].style.display='none';
+        forms[2].style.display='flex';
+    }
+
+    
+});
+});
+
+
