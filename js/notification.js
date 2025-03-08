96077da5692d3b7f662e9610dbb3372796536251
crisjimen <182010261+crisjimen@users.noreply.github.com>
2025-03-08 17:02:02 +0100

    Se modifican algunas clases de tailwind y se añade la sección de notificaciones

---
+++ js/notification.js
@@ -0,0 +1,12 @@
+
+const Notification = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+// Renderiza el componente en el `div#root`
+const root = ReactDOM.createRoot(document.getElementById("notification"));
+root.render(<Notification />);
