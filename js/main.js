a9565e5855cd252a733bcedf4de144265d5b7c41
crisjimen <182010261+crisjimen@users.noreply.github.com>
2025-03-08 20:34:24 +0100

    Se incluye la animación cuando aparece el panel de notificaciones

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ const icon = document.querySelector('[data-icon="iconamoon:notification"]');
 const notificationPanel = document.querySelector('#notification')
 
 icon.addEventListener('click', () => {
-    notificationPanel.classList.toggle('hidden');
-    notificationPanel.classList.toggle('absolute');    
+    notificationPanel.classList.toggle('opacity-0'); 
+    notificationPanel.classList.toggle('translate-y-4');  
 });
