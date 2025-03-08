dab4bcd3448963882520e13827313ba95e2e70a3
crisjimen <182010261+crisjimen@users.noreply.github.com>
2025-03-08 21:48:44 +0100

    Creación de la sección de crear pool

--- js/main.js
+++ js/main.js
@@ -7,3 +7,28 @@ icon.addEventListener('click', () => {
     notificationPanel.classList.toggle('opacity-0'); 
     notificationPanel.classList.toggle('translate-y-4');  
 });
+
+//Mostrar pantalla de crear una nueva pool
+const createPoolCard = document.querySelector('.create-pool');
+const button1 = document.querySelector('.create');
+const button2 = document.querySelector('.home__now-title button');
+const createButtons = [button1, button2];
+const overlay = document.querySelector('#overlay');
+
+createButtons.forEach(button => {
+  button.addEventListener('click', () => {
+      createPoolCard.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+  });  
+});
+
+const closeCard = document.querySelector('#close-card');
+closeCard.addEventListener('click', () => {
+    createPoolCard.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', () => {
+    createPoolCard.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
